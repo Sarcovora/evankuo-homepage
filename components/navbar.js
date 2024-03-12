66a48da3fbe4b65aa4f4f44b98ed9b6a86c91a72
Evan Kuo <evan.kuo.edu@gmail.com>
2024-03-12 17:48:42 -0500

    fixed colormode

--- components/navbar.js
+++ components/navbar.js
@@ -18,21 +18,6 @@ import {
 import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button.js'
 
-// const LinkItem = ({ href, path, children }) => {
-//   const active = path === href
-//   const inactiveColor = useColorModeValue('gray200', 'whiteAlpha.900')
-//   return (
-//     <NextLink
-//       href={href}
-//       p={2}
-//       bg={active ? 'glassTeal' : undefined}
-//       color={active ? '#202023' : inactiveColor}
-//     >
-//       {children}
-//     </NextLink>
-//   )
-// }
-
 const LinkItem = ({ href, path, target, children, ...props }) => {
   const active = path === href
   const inactiveColor = useColorModeValue('gray.800', 'whiteAlpha.900')
@@ -62,7 +47,7 @@ const Navbar = props => {
       w="100%"
       bg={useColorModeValue('#ffffff40', '#20202380')}
       style={{ backdropFilter: 'blur(10px)' }}
-      zIndex={1}
+      zIndex={2}
       {...props}
     >
       <Container
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,42 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
+import NextLink from 'next/link'
+import {
+  Link,
+  Container,
+  Heading,
+  Box,
+  SimpleGrid,
+  Button,
+  List,
+  ListItem,
+  useColorModeValue,
+} from '@chakra-ui/react'
 
 const Page = () => {
-	return (
-		<Container>
-			<Box borderRadius='lg' bg="red" p={3} mb={6} align="center">
-				Hello, I&apos;m a student studying CS at the University of Texas at Austin!
-			</Box>
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        mb={6}
+        p={3}
+        textAlign="center"
+        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+        css={{ backdropFilter: 'blur(10px)' }}
+      >
+        Hello, I&apos;m a student studying CS at the University of Texas at
+        Austin!
+      </Box>
 
-			<Box display={{md:'flex'}}>
-				<Box flexGrow={1}>
-					<Heading as="h2" variant="page-title">
-						Evan Kuo
-					</Heading>
-					<p>Aspiring Software Engineer and AI/ML Researcher</p>
-				</Box>
-			</Box>
-		</Container>
-
-	)
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Evan Kuo
+          </Heading>
+          <p>Aspiring Software Engineer and AI/ML Researcher</p>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Page
+
