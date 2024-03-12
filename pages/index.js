a6c2c5f36d3afbe9ba36497c967f91d116375324
Evan Kuo <evan.kuo.edu@gmail.com>
2024-03-12 18:37:30 -0500

    added more to the home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
-// import NextLink from 'next/link'
+import NextLink from 'next/link'
 import {
-  // Link,
+  Link,
   Container,
   Heading,
   Box,
@@ -8,8 +8,11 @@ import {
   // Button,
   // List,
   // ListItem,
+  Image,
   useColorModeValue,
 } from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
 
 const Page = () => {
   return (
@@ -33,7 +36,62 @@ const Page = () => {
           </Heading>
           <p>Aspiring Software Engineer and AI/ML Researcher</p>
         </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <Image
+              src="/images/kuo.jpg"
+              alt="Profile image"
+              width="100"
+              height="100"
+            />
+          </Box>
+        </Box>
       </Box>
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work &amp; Projects
+        </Heading>
+        <Paragraph>
+          Evan is a student at the University of Texas at Austin who is
+          interested in full-stack development and AI/ML research. Currently, he
+          is a part of the Undergraduate Robot Air Hockey Research Group,
+          focusing on training a robotic arm to play air hockey using
+          Reinforcement Learning. Additionally, he is an engineering fellow in
+          the{' '}
+          <Link
+            as={NextLink}
+            href="https://txproduct.notion.site/Texas-Product-Engineering-Organization-bf30d6c098ac4b3889bb1524194fd4b9"
+            passHref
+            scroll={false}
+          >
+            Texas Product Engineering Organization
+          </Link>{' '}
+          and is working on a web app called{' '}
+          <Link
+            as={NextLink}
+            href="https://thoughtless-teal.vercel.app/"
+            passHref
+            scroll={false}
+          >
+            Thoughtless
+          </Link>{' '}
+          to help organizations process applications easier.
+        </Paragraph>
+      </Section>
     </Container>
   )
 }
