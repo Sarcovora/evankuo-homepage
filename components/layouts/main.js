73353fb03763be2b0aa9023c0fb6a980411cc3e3
Evan Kuo <evan.kuo.edu@gmail.com>
2024-03-12 22:48:49 -0500

    footer and favicons?

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,23 +1,37 @@
 import Head from 'next/head'
 import Navbar from '../navbar.js'
 import { Box, Container } from '@chakra-ui/react'
+import Footer from '../footer'
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Evan Kuo - Homepage</title>
+        <meta name="description" content="Evan's homepage" />
+        <meta name="author" content="Evan Kuo" />
+        <meta name="author" content="Sarcovora" />
+        <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+        <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+        <meta property="og:site_name" content="Evan Kuo" />
+        <meta name="og:title" content="Evan Kuo" />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://www.evankuo.vercel.app/card.png"
+        />
+        <title>Evan Kuo</title>
       </Head>
 
       <Navbar path={router.asPath} />
 
       <Container maxW="container.md" pt={14}>
         {children}
+
+        <Footer />
       </Container>
     </Box>
   )
 }
 
 export default Main
-
--- pages/projects.js
+++ pages/projects.js
@@ -1,5 +1,4 @@
-// import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-import { Container, Heading, SimpleGrid } from '@chakra-ui/react'
+import { Container, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
 import Section from '../components/section'
 import { ProjectsGridItem } from '../components/grid-item'
 
@@ -23,7 +22,90 @@ const Projects = () => {
             for organizations.
           </ProjectsGridItem>
         </Section>
+        {/* <Section> */}
+        {/*   <ProjectsGridItem */}
+        {/*     id="thoughtless" */}
+        {/*     title="Thoughtless." */}
+        {/*     thumbnail={thumbThoughtless} */}
+        {/*   > */}
+        {/*     A web application that makes the application review process easier */}
+        {/*     for organizations. */}
+        {/*   </ProjectsGridItem> */}
+        {/* </Section> */}
+
+        {/* <Section delay={0.1}> */}
+        {/*   <ProjectsGridItem */}
+        {/*     id="thoughtless" */}
+        {/*     title="Thoughtless." */}
+        {/*     thumbnail={thumbThoughtless} */}
+        {/*   > */}
+        {/*     A web application that makes the application review process easier */}
+        {/*     for organizations. */}
+        {/*   </ProjectsGridItem> */}
+        {/* </Section> */}
+        {/* <Section delay={0.1}> */}
+        {/*   <ProjectsGridItem */}
+        {/*     id="thoughtless" */}
+        {/*     title="Thoughtless." */}
+        {/*     thumbnail={thumbThoughtless} */}
+        {/*   > */}
+        {/*     A web application that makes the application review process easier */}
+        {/*     for organizations. */}
+        {/*   </ProjectsGridItem> */}
+        {/* </Section> */}
       </SimpleGrid>
+
+      {/* <Section delay={0.2}> */}
+      {/*   <Divider my={6} /> */}
+      {/**/}
+      {/*   <Heading as="h3" fontSize={20} mb={4}> */}
+      {/*     Team Projects */}
+      {/*   </Heading> */}
+      {/* </Section> */}
+      {/**/}
+      {/* <SimpleGrid columns={[1, 1, 2]} gap={6}> */}
+      {/*   <Section delay={0.3}> */}
+      {/*     <ProjectsGridItem */}
+      {/*       id="thoughtless" */}
+      {/*       title="Thoughtless." */}
+      {/*       thumbnail={thumbThoughtless} */}
+      {/*     > */}
+      {/*       A web application that makes the application review process easier */}
+      {/*       for organizations. */}
+      {/*     </ProjectsGridItem> */}
+      {/*   </Section> */}
+      {/*   <Section delay={0.3}> */}
+      {/*     <ProjectsGridItem */}
+      {/*       id="thoughtless" */}
+      {/*       title="Thoughtless." */}
+      {/*       thumbnail={thumbThoughtless} */}
+      {/*     > */}
+      {/*       A web application that makes the application review process easier */}
+      {/*       for organizations. */}
+      {/*     </ProjectsGridItem> */}
+      {/*   </Section> */}
+      {/**/}
+      {/*   <Section delay={0.3}> */}
+      {/*     <ProjectsGridItem */}
+      {/*       id="thoughtless" */}
+      {/*       title="Thoughtless." */}
+      {/*       thumbnail={thumbThoughtless} */}
+      {/*     > */}
+      {/*       A web application that makes the application review process easier */}
+      {/*       for organizations. */}
+      {/*     </ProjectsGridItem> */}
+      {/*   </Section> */}
+      {/*   <Section delay={0.3}> */}
+      {/*     <ProjectsGridItem */}
+      {/*       id="thoughtless" */}
+      {/*       title="Thoughtless." */}
+      {/*       thumbnail={thumbThoughtless} */}
+      {/*     > */}
+      {/*       A web application that makes the application review process easier */}
+      {/*       for organizations. */}
+      {/*     </ProjectsGridItem> */}
+      {/*   </Section> */}
+      {/* </SimpleGrid> */}
     </Container>
   )
 }
